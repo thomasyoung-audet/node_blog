e667f3be0a7b9e47b55666044adaf81bf42247d1
Thomas <thomasyoungaudet@gmail.com>
2020-03-05 16:26:39 -0500

    Can now add articles in real time

--- client/src/components/Article/Form/index.jsx
+++ client/src/components/Article/Form/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Form extends React.Component {
   constructor(props) {
@@ -16,13 +17,15 @@ class Form extends React.Component {
   }
   //Submit article
   handleSubmit(){
+    const { onSubmit } = this.props;
     const { title, body, author } = this.state;
 
     return axios.post('http://localhost:8000/api/articles', {
       title,
       body,
       author,
-    });
+    })
+      .then((res) => onSubmit(res.data));
   }
 
   //handle input
@@ -61,4 +64,8 @@ class Form extends React.Component {
   }
 }
 
-export default Form;
+const mapDispatchToProps = dispatch => ({
+  onSubmit: data => dispatch({ type: 'SUBMIT_ARTICLE', data }),
+});
+
+export default connect(null, mapDispatchToProps)(Form);
